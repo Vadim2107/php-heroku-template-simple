b99b97ca5ec50e27de1827bb2d126d08bb22ff03
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2021-04-03 23:32:42 +0300

    edit exit.php

--- web/exit.php
+++ web/exit.php
@@ -1,7 +1,7 @@
 <?php
 // $_SESSION = null;
-$_SESSION['username'] = null;
-// session_destroy();
-session_unset();
-echo 'Session-null';
-// header("Location: index.php");
+// $_SESSION['username'] = null;
+session_destroy();
+// session_unset();
+
+header("Location: index.php");
--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 // $_SESSION = null;
-session_start();
+// session_start();
 
 // include_once 'index.html';
 $userName = $_SESSION['username'];
