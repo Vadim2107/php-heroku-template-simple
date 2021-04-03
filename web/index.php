2834635b345668916a1dbd899134bbab98d4472f
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2021-04-03 22:27:41 +0300

    edit index.php

--- web/index.php
+++ web/index.php
@@ -1,13 +1,14 @@
 <?php
 
 session_start();
+echo '<a href="exit.php">Exit</a>';
 // include_once 'index.html';
-// $userName = $_SESSION['username'];
+$userName = $_SESSION['username'];
 // echo '<a href="/exit.php">Exit</a>';
 // echo 'Hello, '.$_SESSION['username'];
-if (isset($_SESSION['username'])) {
+if (isset($userName)) {
     echo 'Hello, '.$userName.'!!!';
-    echo '<a href="/exit.php">Exit</a>';
+    echo '<a href="exit.php">Exit</a>';
 } else {
     include 'index.html';
 }
