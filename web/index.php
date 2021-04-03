2baf59d71860555bb6ea1528f24e7a9674959c6f
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2021-04-03 19:58:40 +0300

    edit index.php

--- web/index.php
+++ web/index.php
@@ -2,8 +2,8 @@
 
 // include_once 'index.html';
 // echo 'Hello, '.$_SESSION['username'];
-if ($_SESSION['username'] !== " ") {
-    echo 'Hello, '.$_SESSION['username'].'!!!';
-} else {
+if ($_SESSION['username'] === " ") {
     include 'index.html';
+} else {
+    echo 'Hello, '.$_SESSION['username'].'!!!';
 }
